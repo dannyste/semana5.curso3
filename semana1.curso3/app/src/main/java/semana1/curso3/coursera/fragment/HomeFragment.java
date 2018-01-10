59156ffcb83d10c1f2741d675e916147d42bc5c6
Danny Ponce <dponceld@gmail.com>
2018-01-10 12:53:23 -0500

    MVP.

--- semana1.curso3/app/src/main/java/semana1/curso3/coursera/fragment/HomeFragment.java
+++ semana1.curso3/app/src/main/java/semana1/curso3/coursera/fragment/HomeFragment.java
@@ -14,18 +14,17 @@ import semana1.curso3.coursera.R;
 import semana1.curso3.coursera.adapter.PetAdapter;
 import semana1.curso3.coursera.pojo.Pet;
 import semana1.curso3.coursera.pojo.PetConstructor;
+import semana1.curso3.coursera.presenter.HomeFragmentPresenter;
+import semana1.curso3.coursera.presenter.IHomeFragmentPresenter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements IHomeFragmentView {
 
     private RecyclerView rv_pet;
 
-    private ArrayList<Pet> pets;
-    private PetAdapter petAdapter;
-
-    private PetConstructor petConstructor;
+    private IHomeFragmentPresenter iHomeFragmentPresenter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -37,14 +36,26 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         rv_pet = (RecyclerView) view.findViewById(R.id.rv_pet);
+        iHomeFragmentPresenter = new HomeFragmentPresenter(getActivity(), this );
+        return view;
+    }
+
+    @Override
+    public void generateLinearLayoutManagerVertical() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         rv_pet.setLayoutManager(linearLayoutManager);
-        petConstructor = new PetConstructor(getActivity());
-        pets = petConstructor.getPets();
-        petAdapter = new PetAdapter(getActivity(), pets);
+    }
+
+    @Override
+    public PetAdapter createPetAdapter(ArrayList<Pet> pets) {
+        PetAdapter petAdapter = new PetAdapter(getActivity(), pets);
+        return petAdapter;
+    }
+
+    @Override
+    public void initializePetAdapter(PetAdapter petAdapter) {
         rv_pet.setAdapter(petAdapter);
-        return view;
     }
 
 }
--- semana1.curso3/app/src/main/java/semana1/curso3/coursera/presenter/IHomeFragmentPresenter.java
+++ semana1.curso3/app/src/main/java/semana1/curso3/coursera/presenter/IHomeFragmentPresenter.java
@@ -2,6 +2,8 @@ package semana1.curso3.coursera.presenter;
 
 public interface IHomeFragmentPresenter {
 
+    void getPets();
 
+    void showPets();
 
 }
